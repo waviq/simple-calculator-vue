25c2218c0bf8f29e764da23f28a0b247d05edcfc
waviq <waviq.subkhi@gmail.com>
2021-11-09 02:07:56 +0700

    simple calculator

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,156 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+        <div class="container">
+            <div class="col-md-4">
+                <h3>Simple Calculator</h3>
+                <table class="table table-bordered">
+                    <tbody>
+                    <tr v-for="calculator in calculators">
+                        <td>
+                            <input type="number" v-model="calculator.number" class="form-control"
+                                   :disabled="disabledStatus(calculator.status)">
+                        </td>
+                        <td>
+                            <input v-model="calculator.status" type="checkbox" @click="checkList(calculator.status)"/>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <button :disabled="disabledButtonOperator" type="button" class="btn btn-primary" @click="tambah()">+</button>
+                            <button :disabled="disabledButtonOperator" type="button" class="btn btn-success" @click="kurang()">-</button>
+                            <button :disabled="disabledButtonOperator" type="button" class="btn btn-info" @click="kali">x</button>
+                            <button :disabled="disabledButtonOperator" type="button" class="btn btn-danger" @click="bagi">/</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2">
+                            <hr/>
+                        </td>
+                    </tr>
+                    <tr style="text-align: left">
+                        <td>Hasil</td>
+                        <td>{{resultTotal}}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        data() {
+            return {
+                number_1: '',
+                number_2: '',
+                number_3: '',
+                calculators: [
+                    {
+                        number: 0,
+                        status: true
+                    },
+                    {
+                        number: 0,
+                        status: true
+                    },
+                    {
+                        number: 0,
+                        status: false
+                    }
+                ],
+                status: [],
+                resultTotal: '',
+                countCheck: parseInt(2)
+
+            }
+        },
+        computed: {
+            disabledButtonOperator()  {
+                return this.countCheck <= 1;
+            }
+        },
+        methods: {
+            tambah() {
+                let result = this.calculators.reduce((sum, data) => {
+                    return sum + Number(data.number)
+                }, 0)
+
+                this.resultTotal = result
+            },
+            kurang() {
+                let number_1 = Number(this.calculators[0].number)
+                let number_2 = Number(this.calculators[1].number)
+                let number_3 = Number(this.calculators[2].number)
+
+                this.resultTotal = number_1 - number_2 - number_3
+            },
+            kali() {
+                let numbers = []
+                this.calculators.forEach((value) => {
+                    if (value.status){
+                        numbers.push(Number(value.number))
+                    }
+                })
+                let result = numbers.reduce((a,b)=> a * b, 1)
+                this.resultTotal = result
+
+
+            },
+            bagi() {
+                let numbers = []
+                let total = []
+                this.calculators.forEach((value) => {
+                    if (value.status){
+                        numbers.push(Number(value.number))
+                    }
+                })
+
+                let result = numbers.reduce((total, number)=> {
+                    return total / number
+                })
+
+                this.resultTotal = result
+
+            },
+            disabledStatus(status) {
+                return status !== true;
+            },
+
+            checkList(status) {
+                if (status) {
+                    this.countCheck = this.countCheck - status
+                } else {
+                    this.countCheck += 1
+                }
+               
+                if (this.countCheck <=1) {
+                    alert('minimum input is 2')
+                }
+            }
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
+    .table-bordered td, .table-bordered th {
+        border: none;
+    }
+
+    .btn {
+        margin-right: 10px;
+    }
+
+    hr {
+        margin-top: 1rem;
+        margin-bottom: 1rem;
+        border: 0;
+        border-top: 1px solid rgba(0, 0, 0, 0.1);
+    }
 </style>
